efd48dc4cc32c071637273dcc4fca86bee9e0951
Vsevolod Romashov <7@7vn.ru>
2011-06-01 23:14:50 +0400

    added nested_comments, js still buggy

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,4 +4,10 @@ module PostsHelper
       date.strftime('%Y.%m.%d | %H:%M')
     end
   end
+  
+  def nested_comments(comments)
+    comments.map do |comment, sub_comments|
+      render(comment) + content_tag(:div, nested_comments(sub_comments), :class => "nested_comments")
+    end.join.html_safe
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
   has_many :comments
   
   validates_presence_of :title, :body
+  
+  def has_comments?
+    comments.size > 0
+  end
 end
