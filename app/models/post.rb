de201cc56854483968ccbbc89dfdc9ff6a4ace8b
Vsevolod Romashov <7@7vn.ru>
2011-06-10 11:07:01 +0400

    archive now shows only months with posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -19,7 +19,7 @@ class Post < ActiveRecord::Base
     (START_MONTH..Time.now.strftime('%Y%m').to_i).each do |month_num|
       month_string = month_num.to_s
       year, month = month_string[0..3], month_string[4..5]
-      (years[year.to_i] ||= []) << month.to_i
+      (years[year.to_i] ||= []) << month.to_i if created_on(year, month).count > 0
     end
     years
   end
